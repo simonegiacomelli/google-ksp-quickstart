7b48c945c674247790a23997003f2af065bb8779
Simone <simone.giacomelli@gmail.com>
2023-01-29 08:58:06 +0000

    fix main

--- workload/src/main/java/com/example/A.kt
+++ workload/src/main/java/com/example/A.kt
@@ -11,7 +11,8 @@ fun main() {
         .withA(1)
         .withB("foo")
         .withC(2.3)
-    val aClass : AClass = builder.build()
+        .withD(hello)
+    val aClass: AClass = builder.build()
     println(aClass.foo())
 }
 
